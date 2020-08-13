79750df7d3b078e9b916866c7e43e70529d4cc14
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-13 19:56:08 -0300

    useEffect Precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,19 +1,35 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 export const Message = () => {
 
+    const [coords, setCooords] = useState({ x:0, y:0})
+
+    const {x , y} = coords
+
+    
+
 
     useEffect(() => {
-        console.log('Componente Montado')
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y};
+           setCooords(coords);
+       }
+   
+       window.addEventListener('mousemove', mouseMove);
+
         return () => {
-            console.log('Componente desmontado')
+
+            window.removeEventListener('mousemove', mouseMove);
         }
     }, [])
 
     return (
         <div>
             <h3>Eres genial</h3>
+            <p>
+                x:{x} y:{y}
+            </p>
         </div>
     )
 }
