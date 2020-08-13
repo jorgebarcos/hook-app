aece53efaf31407c48670c47763cc56f43bcbc35
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-13 19:15:07 -0300

    useEffect - SimpleForm

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react'
+
+import './effects.css'
+
+
+export const SimpleForm = () => {
+
+   const [formState, setFormState] = useState({
+       name: '',
+       email: ''
+   });
+
+   const { name, email } = formState;
+
+
+    useEffect( ()=> {
+        console.log("Hey!")
+    }, []);
+
+    useEffect( ()=> {
+        console.log("formState cambio")
+    }, [formState]);
+
+    useEffect( ()=> {
+        console.log("email cambio")
+    }, [email]);
+
+
+
+    const handleInputChange = ({target}) => {
+        
+        setFormState({
+            ...formState,
+            [ target.name ]: target.value
+        })
+    }
+
+
+
+    return (
+        <>
+            <h1>useEffect</h1>
+            <hr/>
+
+            <div className="form-group">
+                <input 
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Tu Nombre"
+                    autoComplete="off"
+                    value={ name }
+                    onChange={ handleInputChange }
+                />
+            </div>
+
+            <div className="form-group">
+                <input 
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="correo@correo.com"
+                    autoComplete="off"
+                    value={ email }
+                    onChange={ handleInputChange }
+                />
+            </div>
+        </>
+    )
+}
