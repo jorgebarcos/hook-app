870c73836d852cc9efa78655e6814d691a723663
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-14 20:02:53 -0300

    useReducer - Todo List

---
+++ src/components/08-useReducer/TodoApp.js
@@ -0,0 +1,36 @@
+import React, { useReducer } from 'react'
+import { todoReducer } from './todoReducer'
+import './style.css'
+
+
+const initialState = [{
+    id: new Date().getTime(),
+    desc: 'Aprender React',
+    done: false
+}]
+
+
+
+
+export const TodoApp = () => {
+
+    const [todos] = useReducer(todoReducer, initialState)
+
+    console.log(todos)
+
+
+    return (
+        <div>
+            <h1>TodoApp</h1>
+            <hr/>
+
+            <ul>
+                <li>Hola</li>
+                <li>Mundo</li>
+                <li>Hola de Nuevo</li>
+            </ul>
+
+
+        </div>
+    )
+}
