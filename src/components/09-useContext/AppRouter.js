cb401f906512be8b1b6b71c9a4f011ca4ab70f5a
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-15 19:04:27 -0300

    Link y NavLink

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import { NavBar } from './NavBar';
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from './LoginScreen';
 import { HomeScreen } from './HomeScreen';
@@ -13,11 +14,13 @@ export const AppRouter = () => {
     return (
        <Router>
            <div>
-
+                <NavBar />
                 <Switch >
                     <Route exact path="/" component={HomeScreen} />
                     <Route exact path="/about" component={AboutScreen} />
                     <Route exact path="/login" component={LoginScreen} />
+
+                    <Route component={HomeScreen} />
 
                 </Switch>
 
